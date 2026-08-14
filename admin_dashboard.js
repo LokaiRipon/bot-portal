d1f52c1ae337d223d2f63be466bef6f3830de9af
LokaiRipon <lokaieripon@gmail,com>
2026-08-14 05:23:25 -0400

    NEW SERVER 01

--- admin_dashboard.js
+++ admin_dashboard.js
@@ -1,4 +1,4 @@
-const API_BASE = 'hbase-production.up.railway.app';
+const API_BASE = 'https://hbase-production.up.railway.app/';
 let authToken = localStorage.getItem('admin_token');
 let currentLicenses = [];
 
